50407f3ad059ef3b332dce6f77f1c2039dbd4b11
Syed Shayan <3syedshayan@gmail.com>
2025-06-13 23:37:11 +0500

    Added SQL Query

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -2,12 +2,39 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class DatabaseService {
-  static final DatabaseService instance = DatabaseService._constructor();
+  static Database? _db;
+  static final DatabaseService instance =
+      DatabaseService._constructor(); // Creates a singleton instance
+  final String _tasksTableName = "tasks";
+  final String _tasksIdColumnName = "id";
+  final String _tasksContentColumnName = "content";
+  final String _tasksStatusColumnName = "status";
+  DatabaseService._constructor(); // Private constructor for singleton pattern
+  Future<Database> get database async {
+    if (_db != null) return _db!;
+    _db = await getDatabase();
+    return _db!;
+  }
 
-  DatabaseService._constructor();
   Future<Database> getDatabase() async {
-    final databaseDirPath = await getDatabasesPath();
-    final databasePath = join(databaseDirPath, 'master_db.db');
-    final database = await openDatabase(databasePath);
+    final databaseDirPath =
+        await getDatabasesPath(); // Get the default database directory path
+    final databasePath = join(
+      databaseDirPath,
+      'master_db.db',
+    ); // Define the database file name
+    final database = await openDatabase(
+      databasePath,
+      onCreate: (db, version) {
+        db.execute('''
+          CREATE TABLE $_tasksTableName {
+          $_tasksIdColumnName INTEGER PRIMARY KEY,
+          $_tasksContentColumnName TEXT NOT NULL,
+          $_tasksStatusColumnName INTEGER NOT NULL,
+          }
+          ''');
+      },
+    ); // Open or create the database
+    return database;
   }
 }
